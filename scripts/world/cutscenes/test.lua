ab6962452869bc7341b327a97e504dfbcd73910b
kriscrossapplesauce27 <lorenzodragusanu9@gmail.com>
2026-07-21 20:24:03 +0200

    fixed text color to be accurate to DELTARUNE
    (updated only test cutscene for now)
    and added other flower minifaces and colors

--- scripts/world/cutscenes/test.lua
+++ scripts/world/cutscenes/test.lua
@@ -3,8 +3,9 @@ return {
         local aqua = cutscene:getCharacter("aqua")
         if aqua then
             cutscene:setSpeaker("aqua")
-            aqua:setSprite("pose")
-            cutscene:text("[miniface:aqua][color:#55ffff]* Uuu")
+            aqua:setAnimation("sleevewave")
+            cutscene:text("[miniface:aqua][color:#65f8ff]aqua\n[miniface:seth][color:#dc93fc]seth\n[miniface:yellow][color:#fff68a]yellow")
+            cutscene:text("[miniface:orange][color:#ff9769]orange\n[miniface:green][color:#9affab]green\n[miniface:blue][color:#6891ff]blue ")
             aqua:resetSprite()
 
         end
